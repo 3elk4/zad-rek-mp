1fd44547959b4a2e513644d5c579fbc748cc5536
3elk4 <izaa1109@gmail.com>
2024-03-29 19:30:23 +0100

    add validations to models

--- MediportaZadRek/MediportaZadRek/Models/Tag.cs
+++ MediportaZadRek/MediportaZadRek/Models/Tag.cs
@@ -1,10 +1,21 @@
-﻿namespace MediportaZadRek.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediportaZadRek.Models
 {
     public class Tag
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [RegularExpression(@"^\d+$")]
+        [Range(0, 9999999999999999.99)]
         public decimal Count { get; set; }
+
+        [RegularExpression(@"^\d+([\.,]\d{1,2})?$")]
+        [Range(0, 9999999999999999.99)]
         public decimal PercentagePopulation { get; set; }
     }
 }
--- MediportaZadRek/MediportaZadRek/QCRS/Tag/TagsWithPaginationDetails.cs
+++ MediportaZadRek/MediportaZadRek/QCRS/Tag/TagsWithPaginationDetails.cs
@@ -1,10 +1,18 @@
-﻿namespace MediportaZadRek.QCRS.Tag
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediportaZadRek.QCRS.Tag
 {
     public class TagsWithPaginationDetails
     {
         public List<Models.Tag> Items { get; set; } = new List<Models.Tag>();
+
+        [Range(0, 2147483647)]
         public int CurrentPage { get; set; }
+
+        [Range(0, 2147483647)]
         public int PageSize { get; set; }
+
+        [Range(0, 2147483647)]
         public int Total { get; set; }
     }
 }
